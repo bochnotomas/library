3655d54bf4dcbd6f553e4f92fc1605ab389b683c
bochnotomas <tomasbochnovic76@gmail.com>
2021-03-06 18:34:05 +0000

    Books adding function integrated with the ui

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const listOfBooks = document.querySelector(".library")
+const popup = document.querySelector(".popup")
 let myLibrary = []
 let book
 
+//book object
 function Book(author, title, numberOfPages, read) {
     this.author = author
     this.title = title
@@ -8,13 +11,48 @@ function Book(author, title, numberOfPages, read) {
     this.read = read
 }
 
+//popup form code
+function toggleForm() {
+    popup.classList.toggle("hidden")
+}
+
+
+//form validation
+const form = document.querySelector("#addBook")
+const title = document.querySelector("#title")
+const author = document.querySelector("#author")
+const pages = document.querySelector("#pages")
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    addBookToLibrary(author.value, title.value, pages.value, false)
+    displayLibrary()
+    toggleForm()
+})
+
+//adding a book object to library array function
 function addBookToLibrary(author, title, n) {
     book = new Book(author, title, n, false)
     myLibrary.push(book)
 }
 
 
+
+//dummy data
 addBookToLibrary("In Search of Lost Time", "Marcel Proust", 200)
 addBookToLibrary("Ulysses", "James Joyce", 300)
 addBookToLibrary("Don Quixote", "Miguel de Cervantes", 400)
 
+
+//displaying list of books
+function displayLibrary() {
+    listOfBooks.innerHTML = ""
+    myLibrary.forEach(book => {
+        listOfBooks.innerHTML += `<div> ${book.title}, ${book.author}, ${book.numberOfPages} </div>`
+    })
+}
+
+
+displayLibrary()
